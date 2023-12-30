ffcbb2d7df398c448703abcd4a9fb070ebcf07b7
Siddarth <pullakhandam.siddartha@gmail.com>
2023-12-29 20:46:20 -0500

    commit

--- StoryWriter/app.py
+++ StoryWriter/app.py
@@ -1,77 +1,87 @@
-# from dotenv import load_dotenv
-# load_dotenv()
-# import streamlit as st
-# import os
-# import google.generativeai as genai
-# from langchain_google_genai import ChatGoogleGenerativeAI
-# from langchain.prompts import PromptTemplate
+import os
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+# Load environment variables from .env file
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model_gemini = ChatGoogleGenerativeAI(model="gemini-pro")
 
-# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-# model = ChatGoogleGenerativeAI(model="gemini-pro-vision")
 
-
-# # Streamlit app
-# def main():
-#     st.title("Interactive Storytelling Platform")
-
-#     # User input
-#     starting_point = st.text_area("Input a starting point for the story:")
-#     uploaded_image = st.file_uploader("Upload a relevant image:", type=["jpg", "png", "jpeg"])
-
-#     if st.button("Generate Story"):
-#         if starting_point:
-#             # Generate story text using GPT-3
-#             story_text = generate_story(starting_point)
-
-#             # Display the generated story
-#             st.markdown("## Generated Story:")
-#             st.write(story_text)
-
-#             # Display the uploaded image
-#             if uploaded_image:
-#                 st.markdown("## Uploaded Image:")
-#                 st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
-
-#         else:
-#             st.warning("Please provide a starting point for the story.")
-
-# # Function to generate story using GPT-3
-# def generate_story(starting_point):
-#     prompt = f"Once upon a time, {starting_point}."
+def model(genre,no_of_characters,location,characters_prompt,hook_prompt,climax_prompt):
+    # here we are creating a template for the prompt
+    main_c = """
+    Crafting a compelling short film necessitates a concise and straightforward premise, a key ingredient for success when storytelling within the constraints of a limited timeframe. The best short films often feature a main character with a specific goal or a tight deadline, such as two friends embarking on a bike ride, a woman seeking new friendship, or a grieving son delivering a eulogy. With time being a precious commodity, short films cannot afford to delve extensively into character backstories. Instead, filmmakers must discern the essential information required to engage the audience in the hero's journey. 
+    Details like an ex-husband may prove extraneous and dilute the impact of the narrative.Moreover, judiciously selecting the number of characters is pivotal. 
+    Each character, whether a protagonist, supporting role, or minor part, should serve a purpose directly tied to the central goal. 
+    If a character's exclusion doesn't impede the main character's progression, 
+    it might be prudent to omit them. Many successful short films unfold within a singular location, not only to streamline the story but also to mitigate budgetary constraints, 
+    particularly for independent filmmakers. "Sam Did It," a renowned 10-minute short set entirely in a morgue operating room, exemplifies how a confined space can intensify the storytelling experience.
+     A useful exercise during brainstorming involves formulating a premise and identifying a single location, prompting creators to contemplate whether the entire narrative can unfold within those confines. 
+    This approach not only sharpens the focus but also aligns with budget considerations for those contemplating a self-produced project.
     
-#     response = model.invoke(prompt)
-#     return response.content
+    """
 
-# if __name__ == "__main__":
-#     main()
-from flask import Flask, render_template, request
-import random
+    template = main_c +"""
+    write a short film story in {genre} genre with {no_of_characters} characters in {location} location
+    Consider being economical with characters and backstory There are {characters_prompt} characters and their behaviours. This is the hook {hook_prompt} 
+    and this is the climax {climax_prompt}
+    """
 
-app = Flask(__name__)
+    # here we are creating a prompt using the template and the input variables
+    prompt = PromptTemplate(input_variables=["genre","no_of_characters","location","characters_prompt","hook_prompt","climax_prompt"],template=template)
 
-def roll_dice():
-    return random.randint(1, 6)
+    # here we are generating the blog
+    response = model_gemini.invoke(prompt.format(genre=genre,no_of_characters=no_of_characters,location=location,characters_prompt=characters_prompt,hook_prompt=hook_prompt,climax_prompt=climax_prompt))
+    print(response)
+    return response.content
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def short_film_story_generator(genre):
+    st.title("Short Film Story Generator")
 
-@app.route('/play', methods=['POST'])
-def play_game():
-    player1_name = request.form['player1']
-    player2_name = request.form['player2']
+    # Prompt for being economical with characters and backstory
+    no_of_characters = st.text_input("How many character do you want? (leave empty for default of 4 characters):")
+    if not no_of_characters:
+        # Default characters if the user leaves it empty
+        no_of_characters = "The story follows four characters who..."
+    # Prompt for minimal locations
+    location = st.text_input("Think about keeping the locations to a minimum. Specify a key location for your short film (leave empty for random location):")
+    if not location:
+        # Default location if the user leaves it empty
+        location = "You can use any location you want"
 
-    player1_roll = roll_dice()
-    player2_roll = roll_dice()
+    # Prompt for interesting characters
+    characters_prompt = st.text_input("Describe interesting traits or quirks of the main characters:")
+    if not characters_prompt:
+        characters_prompt = "You can use any interesting traits or quirks you want"
 
-    winner = None
-    if player1_roll > player2_roll:
-        winner = player1_name
-    elif player2_roll > player1_roll:
-        winner = player2_name
+    # Prompt for the hook
+    hook_prompt = st.text_input("Create a hook for your short film:")
+    if not hook_prompt:
+        # Default hook if the user leaves it empty
+        hook_prompt = "You can use any hook you want"
 
-    return render_template('result.html', player1=player1_name, roll1=player1_roll,
-                           player2=player2_name, roll2=player2_roll, winner=winner)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    # Prompt for the climax
+    climax_prompt = st.text_input("Develop a great climax for your short film:")
+    if not climax_prompt:
+        climax_prompt = "You can use climax you want but should match with the genre"
+
+
+    if st.button("Generate Story"):
+        response = model(genre,no_of_characters,location,characters_prompt,hook_prompt,climax_prompt)
+
+        # Extract the AI's response and display it
+        ai_response = response
+        st.write(f"{ai_response}")
+
+if __name__ == "__main__":
+    genres = ["Sci-Fi", "Drama", "Comedy", "Thriller", "Mystery", "Fantasy", "Horror", "Romance", "Action", "Adventure"]
+    
+    # Create a dropdown menu for genres
+    user_genre = st.sidebar.selectbox("Select the genre:", genres, index=genres.index("Sci-Fi"))
+
+    short_film_story_generator(user_genre)
